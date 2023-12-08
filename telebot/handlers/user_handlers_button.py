c2d97357ea5c85534782f85f248c14c1084fc2e9
ÐAlexander Magomedov <sistem4ik@mail.ru>
2023-12-08 17:11:59 +0500

    add /store_list

--- telebot/handlers/user_handlers_button.py
+++ telebot/handlers/user_handlers_button.py
@@ -1,31 +1,51 @@
 from aiogram import F, Router
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
-from telebot.lexicon.lexicon_ru import LEXICON_RU
 
+from telebot.keyboards.keyboards import create_store_list_keyboard
+from telebot.lexicon.lexicon_ru import LEXICON_RU
+import sqlite3
 
 router = Router()
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
-# и отправлять ему приветственное сообщение показывать кнопки главного меню
+# и отправлять ему приветственное сообщение
 @router.message(CommandStart())
 async def process_start_command(message: Message):
-
-    if str(message.from_user.id) in ('1851454482',):
+    if telegram_id_exist(message):
         await message.answer(LEXICON_RU['/start_good'])
     else:
         await message.answer(f'{LEXICON_RU["/start_bad"]} {message.from_user.id}')
 
 
-# # Этот хэндлер будет срабатывать на нажатие инлайн-кнопки
-# # "Назад"
-# @router.callback_query(F.data == '/start')
-# async def process_start(callback: CallbackQuery):
-#     await callback.message.edit_text(text=LEXICON_RU['/start'], reply_markup=create_menu_keyboard())
-#     await callback.answer()
-#
-#
+# Проверяет что пользователь есть в списке сотрудников
+def telegram_id_exist(message) -> bool:
+    if str(message.from_user.id) in give_all_telegram_id():
+        return True
+
+
+# Функция возвращает полный список всех телеграм ид сотрудников
+def give_all_telegram_id():
+    connect = sqlite3.connect('db.sqlite3')
+    cursor = connect.cursor()
+    user_list_all = (list(map(lambda x: x[0], cursor.execute("SELECT telegram_id FROM telebot_user").fetchall())))
+    connect.close()
+    return user_list_all
+
+
+# Этот хэндлер будет срабатывать на команду "/store_list"
+# и отправлять пользователю список инлайн кнопок с названиями магазинов
+@router.message(Command(commands='store_list'))
+async def process_about_me_command(message: Message):
+    await message.answer(LEXICON_RU[message.text], reply_markup=create_store_list_keyboard())
+
+
+
+
+
+
+
 # # Этот хэндлер будет срабатывать на нажатие инлайн-кнопки
 # # "Обо мне" в главном меню показывает сообщение и кнопку "назад"
 # @router.callback_query(F.data == '/about_me')
@@ -33,104 +53,10 @@ async def process_start_command(message: Message):
 #     await callback.message.edit_text(text=LEXICON_RU['/about_me'], reply_markup=create_back_keyboard('/start'))
 #     await callback.answer()
 #
-#
-# # Этот хэндлер будет срабатывать на нажатие инлайн-кнопки
-# # "Поддержка" в главном меню показывает сообщение и клавиатуру меню "Поддержка"
-# @router.callback_query(F.data == '/help')
-# async def process_help(callback: CallbackQuery):
-#     await callback.message.edit_text(text=LEXICON_RU['/help'], reply_markup=create_help_keyboard())
-#     await callback.answer()
-#
-#
-# # Этот хэндлер будет срабатывать на нажатие инлайн-кнопки
-# # "Читаем сказку" в главном меню
-# @router.callback_query(F.data == '/read')
-# async def process_read(callback: CallbackQuery):
-#     start = 0
-#     await callback.message.edit_text(
-#         text=LEXICON_RU['/read'],
-#         reply_markup=create_story_list_keyboard(start))
-#     await callback.answer()
-#
-#
 # # Этот хэндлер будет срабатывать на нажатие инлайн-кнопки
 # # "Наша почта" в меню "Поддержка" выводить почту и кнопку "назад"
 # @router.callback_query(F.data == 'email')
 # async def process_email(callback: CallbackQuery):
 #     await callback.message.edit_text(
 #         text=LEXICON_RU['email_send'], reply_markup=create_back_keyboard('/help'))
-#     await callback.answer()
-#
-#
-# # Этот хэндлер будет срабатывать на нажатие инлайн-кнопки
-# # "Оплатить" в меню "Поддержка" выводить почту и кнопку "назад"
-# @router.callback_query(F.data == 'payment')
-# async def process_payment(callback: CallbackQuery):
-#     await callback.message.edit_text(
-#         text=LEXICON_RU['payment'], reply_markup=create_back_keyboard('/help'))
-#     await callback.answer()
-#
-#
-# # Этот хэндлер будет срабатывать на нажатие инлайн-кнопки
-# # ">>" в меню выбора сказки
-# @router.callback_query((F.data).split()[0] == 'forward')
-# async def process_payment(callback: CallbackQuery):
-#     page = int(callback.data.split()[1]) + 1
-#     if page == len(page_list()):
-#         page = 0
-#     await callback.message.edit_text(
-#         text=LEXICON_RU['/read'],
-#         reply_markup=create_story_list_keyboard(page))
-#     await callback.answer()
-#
-#
-# # Этот хэндлер будет срабатывать на нажатие инлайн-кнопки
-# # "<<" в меню выбора сказки
-# @router.callback_query((F.data).split()[0] == 'backward')
-# async def process_payment(callback: CallbackQuery):
-#     page = int(callback.data.split()[1]) - 1
-#     if page < 0:
-#         page = len(page_list()) - 1
-#     await callback.message.edit_text(
-#         text=LEXICON_RU['/read'],
-#         reply_markup=create_story_list_keyboard(page))
-#     await callback.answer()
-#
-#
-# # Этот хэндлер будет срабатывать на нажатие инлайн-кнопки
-# # в списке доступных сказок Запускать первую страницу сказки
-# @router.callback_query(IsStory())
-# async def process_cancel_press(callback: CallbackQuery):
-#     start = 0
-#     await callback.message.edit_text(
-#         text=page_book(callback.data)[start],
-#         reply_markup=create_story_keyboard(start, callback.data))
-#     await callback.answer()
-#
-#
-# # Этот хэндлер будет срабатывать на нажатие инлайн-кнопки
-# # ">>" во время просмотра сказки
-# @router.callback_query((F.data).split()[0] == 'fstory')
-# async def process_payment(callback: CallbackQuery):
-#     page = int(callback.data.split()[1])
-#     book_name = ' '.join(callback.data.split()[2:])
-#     if page < len(page_book(book_name))-1:
-#         page += 1
-#         await callback.message.edit_text(
-#             text=page_book(book_name)[page],
-#             reply_markup=create_story_keyboard(page, book_name))
-#     await callback.answer()
-#
-#
-# # Этот хэндлер будет срабатывать на нажатие инлайн-кнопки
-# # "<<" во время просмотра сказки
-# @router.callback_query((F.data).split()[0] == 'bstory')
-# async def process_payment(callback: CallbackQuery):
-#     page = int(callback.data.split()[1])
-#     book_name = ' '.join(callback.data.split()[2:])
-#     if page > 0:
-#         page -= 1
-#         await callback.message.edit_text(
-#             text=page_book(book_name)[page],
-#             reply_markup=create_story_keyboard(page, book_name))
 #     await callback.answer()
--- telebot/keyboards/keyboards.py
+++ telebot/keyboards/keyboards.py
@@ -1,50 +1,69 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from telebot.lexicon.lexicon_ru import LEXICON_RU
-from telebot.books.book_list import page_list
+import sqlite3
 
 
-# Функция создания инлайн кнопки "Назад" в главном меню
-def create_back_keyboard(arg) -> InlineKeyboardMarkup:
-    # Создаем объект клавиатуры
-    kb_builder = InlineKeyboardBuilder()
-    # Наполняем клавиатуру кнопками-закладками в порядке возрастания
-    kb_builder.row(InlineKeyboardButton(
-        text=LEXICON_RU['back'],
-        callback_data=arg))
-    return kb_builder.as_markup()
-
-
-# Функция создания инлайн кнопкок со списком сказок
-def create_story_list_keyboard(page) -> InlineKeyboardMarkup:
+# Функция создания инлайн кнопкок со списком магазинов
+def create_store_list_keyboard() -> InlineKeyboardMarkup:
     # Создаем объект клавиатуры
     kb_builder = InlineKeyboardBuilder()
     # Наполняем клавиатуру кнопками-закладками
     kb_builder.row(*[InlineKeyboardButton(
         text=story_name,
-        callback_data=story_name) for story_name in page_list()[page]], width=1)
-    kb_builder.row(*[InlineKeyboardButton(
-        text=text,
-        callback_data=comand)
-        for text, comand in ((LEXICON_RU['backward'], (f'backward {page}')), (LEXICON_RU['forward'], (f'forward {page}')))], width=2)
-    kb_builder.row(InlineKeyboardButton(
-        text=LEXICON_RU['back'],
-        callback_data='/start'))
+        callback_data=story_name) for story_name in give_all_store()], width=1)
     return kb_builder.as_markup()
 
 
-def create_story_keyboard(page, book_name) -> InlineKeyboardMarkup:
-    # Создаем объект клавиатуры
-    kb_builder = InlineKeyboardBuilder()
-    # Наполняем клавиатуру кнопками-закладками
-    kb_builder.row(*[InlineKeyboardButton(
-        text=text,
-        callback_data=comand)
-        for text, comand in ((LEXICON_RU['backward'],
-                              (f'bstory {page} {book_name}')),
-                             (LEXICON_RU['forward'],
-                              (f'fstory {page} {book_name}')))], width=2)
-    kb_builder.row(InlineKeyboardButton(
-        text=LEXICON_RU['back'],
-        callback_data='/read'))
-    return kb_builder.as_markup()
+# Функция возвращает полный список всех телеграм ид сотрудников
+def give_all_store():
+    connect = sqlite3.connect('db.sqlite3')
+    cursor = connect.cursor()
+    store_list = (list(map(lambda x: x[0], cursor.execute("SELECT store_name FROM telebot_store").fetchall())))
+    connect.close()
+    return store_list
+
+
+# # Функция создания инлайн кнопки "Назад" в главном меню
+# def create_back_keyboard(arg) -> InlineKeyboardMarkup:
+#     # Создаем объект клавиатуры
+#     kb_builder = InlineKeyboardBuilder()
+#     # Наполняем клавиатуру кнопками-закладками в порядке возрастания
+#     kb_builder.row(InlineKeyboardButton(
+#         text=LEXICON_RU['back'],
+#         callback_data=arg))
+#     return kb_builder.as_markup()
+#
+#
+# # Функция создания инлайн кнопкок со списком сказок
+# def create_story_list_keyboard(page) -> InlineKeyboardMarkup:
+#     # Создаем объект клавиатуры
+#     kb_builder = InlineKeyboardBuilder()
+#     # Наполняем клавиатуру кнопками-закладками
+#     kb_builder.row(*[InlineKeyboardButton(
+#         text=story_name,
+#         callback_data=story_name) for story_name in page_list()[page]], width=1)
+#     kb_builder.row(*[InlineKeyboardButton(
+#         text=text,
+#         callback_data=comand)
+#         for text, comand in ((LEXICON_RU['backward'], (f'backward {page}')), (LEXICON_RU['forward'], (f'forward {page}')))], width=2)
+#     kb_builder.row(InlineKeyboardButton(
+#         text=LEXICON_RU['back'],
+#         callback_data='/start'))
+#     return kb_builder.as_markup()
+#
+#
+# def create_story_keyboard(page, book_name) -> InlineKeyboardMarkup:
+#     # Создаем объект клавиатуры
+#     kb_builder = InlineKeyboardBuilder()
+#     # Наполняем клавиатуру кнопками-закладками
+#     kb_builder.row(*[InlineKeyboardButton(
+#         text=text,
+#         callback_data=comand)
+#         for text, comand in ((LEXICON_RU['backward'],
+#                               (f'bstory {page} {book_name}')),
+#                              (LEXICON_RU['forward'],
+#                               (f'fstory {page} {book_name}')))], width=2)
+#     kb_builder.row(InlineKeyboardButton(
+#         text=LEXICON_RU['back'],
+#         callback_data='/read'))
+#     return kb_builder.as_markup()
