0eed37c656b0b61950014b49908f385e39548b2b
ÐAlexander Magomedov <sistem4ik@mail.ru>
2023-12-10 21:53:59 +0500

    bagfix CompletedOrder

--- telebot/models.py
+++ telebot/models.py
@@ -52,7 +52,7 @@ class Order(models.Model):
 
 
 class CompletedOrder(models.Model):
-    store = models.OneToOneField(to=Store, related_name='completed_store', on_delete=models.CASCADE, unique=False)
+    store = models.ForeignKey(to=Store, related_name='completed_store', on_delete=models.CASCADE)
     user = models.ForeignKey(to=User, related_name='order_user', on_delete=models.CASCADE)
     data = models.DateTimeField()
     massa = models.PositiveIntegerField(default=0)
