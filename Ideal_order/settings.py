f4adfaf73c794c20ce33e48ae09d390a1c72c0e2
ÐAlexander Magomedov <sistem4ik@mail.ru>
2023-12-15 12:36:59 +0500

    add static files

--- Ideal_order/settings.py
+++ Ideal_order/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bzyx3=e@2xkt$m_0kpedjjcd7z20ho09v+!@uvu#%e%w0s9+_@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', '192.168.0.66']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
